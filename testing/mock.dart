033e92de441838aea19cf62ceacbf20515578822
junki-pw <juken.master1234@gmail.com>
2025-01-15 17:01:26 +0900

    mock 作成

---
+++ testing/mock.dart
@@ -0,0 +1,9 @@
+import 'package:architecture_template/data/services/api/model/booking/booking_api_model.dart';
+
+final kBookingApiModel = BookingApiModel(
+  id: 0,
+  name: 'name',
+  description: 'description',
+  createdAt: DateTime.now(),
+  updatedAt: DateTime.now(),
+);
