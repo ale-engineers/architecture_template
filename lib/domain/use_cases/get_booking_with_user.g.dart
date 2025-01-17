a87580ba05fd2b0046bdd0270082bdb4a95aba54
junki-pw <juken.master1234@gmail.com>
2025-01-17 14:59:09 +0900

    微細な更新

---
+++ lib/domain/use_cases/get_booking_with_user.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_booking_with_user.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getBookingWithUserHash() =>
+    r'572caa687efdcbc0d3364a8bc269d1ea0c54b913';
+
+/// See also [getBookingWithUser].
+@ProviderFor(getBookingWithUser)
+final getBookingWithUserProvider =
+    AutoDisposeProvider<GetBookingWithUser>.internal(
+  getBookingWithUser,
+  name: r'getBookingWithUserProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getBookingWithUserHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GetBookingWithUserRef = AutoDisposeProviderRef<GetBookingWithUser>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
